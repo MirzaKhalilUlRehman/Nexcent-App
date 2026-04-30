130267f01a0411b90b3dbf79a398776eec01ebe7
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-04-30 14:02:04 +0500

    Create UI of Footer Section

--- Nexcent App/src/Nexcent Component/Footer.jsx
+++ Nexcent App/src/Nexcent Component/Footer.jsx
@@ -1,11 +1,9 @@
 function Footer() {
-    return (
-        <div className="footer">
-            <div className="footer-content">
-                <h2>Nexcent</h2>
-                <p>© 2024 Nexcent. All rights reserved.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h5>Pellentesque suscipit fringilla libero eu.</h5>
+      <button>Get a Demo →</button>
+    </div>
+  );
 }
-export default Footer;  
+export default Footer;
