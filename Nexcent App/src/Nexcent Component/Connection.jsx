7f3c5a56fa3481ad7e02da239f782d45951a0cb2
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-04-30 12:26:53 +0500

    Create UI of Connection Section

--- Nexcent App/src/Nexcent Component/Connection.jsx
+++ Nexcent App/src/Nexcent Component/Connection.jsx
@@ -1,8 +1,22 @@
 function Connection() {
-    return (
-        <div className="connection">
-          
-        </div>
-    );
+  return (
+    <div className="connection">
+      <div className="connection-image">
+        <img src="/assets/Connection.png" alt="Connection" />
+      </div>
+      <div className="connection-text">
+        <h4>The unseen of spending three years at Pixelgrade</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+          justo ipsum. Sed accumsan quam vitae est varius fringilla.
+          Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
+          tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.
+          Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec
+          elementum pulvinar odio.
+        </p>
+        <button className="btn">Learn More</button>
+      </div>
+    </div>
+  );
 }
 export default Connection;
