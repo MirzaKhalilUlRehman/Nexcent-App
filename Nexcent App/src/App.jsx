d790ece531b8b48561a943921c9de0eea29d120f
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-04-30 13:56:44 +0500

    Add Footer

--- Nexcent App/src/App.jsx
+++ Nexcent App/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Community />
       <Connection />
       <Achivements />
+      <Connection/>
     </>
   );
 }
---
+++ Nexcent App/src/Nexcent Component/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <h2>Nexcent</h2>
+                <p>© 2024 Nexcent. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+export default Footer;  
