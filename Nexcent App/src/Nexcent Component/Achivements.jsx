897284ce03424d905684146497c3f19a8b542af3
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-04-30 13:15:50 +0500

    Add UI of Achivements

--- Nexcent App/src/Nexcent Component/Achivements.jsx
+++ Nexcent App/src/Nexcent Component/Achivements.jsx
@@ -1,9 +1,35 @@
 function Achivements() {
-    return (
-        <div className="achivements">
-            <h1>Achivements</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+  return (
+    <div className="achivement-section">
+      <h4 className="achivement-heading">Achivements</h4>
+      <div className="achivement-section-text">
+        <p>Helping a local</p>
+        <h3>business reinvent itself</h3>
+        <p>We reached here with our hard work and dedication</p>
+      </div>
+      <div className="achivements">
+        <div className="achivement">
+          <img src="/assets/achievement1.png" alt="Achievement 1" />
+          <p>2,245,341</p>
+          <p>Members</p>
         </div>
-    );
+        <div className="achivement">
+          <img src="/assets/achievement2.png" alt="Achievement 2" />
+          <p>1,926,4361</p>
+          <p>Event Bookings</p>
+        </div>
+           <div className="achivement">
+          <img src="/assets/achievement3.png" alt="Achievement 3" />
+          <p>46,328</p>
+          <p>Payments</p>
+        </div>
+           <div className="achivement">
+          <img src="/assets/achievement4.png" alt="Achievement 4" />
+          <p>828,867</p>
+          <p>Clubs</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Achivements;
